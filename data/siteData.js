8336ce99869b19c52d8dd61b43c9355d8a39a1ba
Darian Nocera <darnocer@gmail.com>
2023-10-29 13:37:07 -0600

    site data file

---
+++ data/siteData.js
@@ -0,0 +1,31 @@
+const siteData = {
+  title: "Darian's Photo Gallery",
+  author: "Darian",
+  description: "Photos from my recent adventures",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://darian.blog",
+  siteRepo: "https://github.com/darnocer/blog",
+  siteLogo: "/static/images/logo.png",
+  image: "/static/images/avatar.png",
+  socialBanner: "/static/images/twitter-card.png",
+  website: "https://darian.digital",
+  email: "darian@madebyrvg.com",
+  github: "https://github.com/darnocer",
+  twitter: "https://twitter.com/Twitter",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com/in/darian-nocera",
+  locale: "en-US",
+  content: {
+    heading: "My Adventures",
+    description:
+      "Just a collection of photos of some of my recent adventures because why not",
+    button: {
+      link: "https://darian.digital",
+      text: "Connect with me",
+    },
+  },
+};
+
+module.exports = siteData;
